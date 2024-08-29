03afad5b1ee1b3cd017fd19a52c5e046abb3a79a
Robert Jank <robert.jank@yahoo.com>
2024-08-29 17:30:33 -0500

    added slice and store.

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,18 @@
-import './styles/reset.css'
-import './styles/styles.css'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App'
+import "./styles/reset.css";
+import "./styles/styles.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
 
-const domNode = document.getElementById('root')
-const root = createRoot(domNode)
+import { Provider } from "react-redux";
+import { store } from "./state/store";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
 // ✨ wrap <App /> with a provider
 root.render(
-  <App />
-)
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,1 +1,9 @@
 // ✨ create your `store` in this module
+import { configureStore } from "@reduxjs/toolkit";
+import slice from "./quotesSlice";
+
+export const store = configureStore({
+  reducer: {
+    state: slice,
+  },
+});
